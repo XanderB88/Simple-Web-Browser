5dff009196b877f5c35d1476fe1490c7dda7397b
XanderB88 <alexxanderbykov@gmail.com>
2021-09-26 18:07:36 +0400

    Add a README.md and a .gitignore files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Web Browser
+### Learning project for learning WebKit.
---
+++ Simple Web Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Web Browser
+//
+//  Created by Alexander on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
